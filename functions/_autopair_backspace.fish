938f6b761729709b1822c1384ba9654694108a73
Jorge Bucaran <mail@jorgebucaran.com>
2021-01-05 00:15:31 +0900

    Split long lines after operators

--- functions/_autopair_backspace.fish
+++ functions/_autopair_backspace.fish
@@ -2,8 +2,8 @@ function _autopair_backspace
     set --local index (commandline --cursor)
     set --local buffer (commandline)
 
-    test $index -ge 1 \
-        && contains -- (string sub --start $index --length 2 "$buffer") $autopair_pairs \
-        && commandline --function delete-char
+    test $index -ge 1 &&
+        contains -- (string sub --start $index --length 2 "$buffer") $autopair_pairs &&
+        commandline --function delete-char
     commandline --function backward-delete-char
 end
