bbd9971e0e864908896734e68b8ff0bb6546eca3
Jorge Bucaran <mail@jorgebucaran.com>
2021-01-05 00:09:54 +0900

    Rebind pairs on fish_key_bindings changes

--- conf.d/autopair.fish
+++ conf.d/autopair.fish
@@ -4,27 +4,34 @@ set --global autopair_left "(" "[" "{" '"' "'"
 set --global autopair_right ")" "]" "}" '"' "'"
 set --global autopair_pairs "()" "[]" "{}" '""' "''"
 
-test $fish_key_bindings = fish_default_key_bindings \
-    && set --local mode default \
-    || set --local mode insert
+function _autopair_fish_key_bindings --on-variable fish_key_bindings
+    test $fish_key_bindings = fish_default_key_bindings &&
+        set --local mode default insert ||
+        set --local mode insert default
 
-bind --mode $mode \177 _autopair_backspace # macOS ⌫
-bind --mode $mode \b _autopair_backspace
-bind --mode $mode \t _autopair_tab
+    bind --mode $mode[-1] --erase \177 \b \t
 
-printf "%s\n" $autopair_pairs | while read --local left right --delimiter ""
-    if test $left = $right
-        bind --mode $mode $left "_autopair_insert_same \\$left"
-    else
-        bind --mode $mode $left "_autopair_insert_left \\$left \\$right"
-        bind --mode $mode $right "_autopair_insert_right \\$right"
+    bind --mode $mode[1] \177 _autopair_backspace # macOS ⌫
+    bind --mode $mode[1] \b _autopair_backspace
+    bind --mode $mode[1] \t _autopair_tab
+
+    printf "%s\n" $autopair_pairs | while read --local left right --delimiter ""
+        bind --mode $mode[-1] --erase $left $right
+        if test $left = $right
+            bind --mode $mode[1] $left "_autopair_insert_same \\$left"
+        else
+            bind --mode $mode[1] $left "_autopair_insert_left \\$left \\$right"
+            bind --mode $mode[1] $right "_autopair_insert_right \\$right"
+        end
     end
 end
 
+_autopair_fish_key_bindings
+
 function _autopair_uninstall --on-event autopair_uninstall
-    bind --all \
-        | string match --regex -- .\*_autopair \
-        | string replace -- _autopair --erase \
-        | source
-    set --names | string replace --filter --regex -- "^(autopair)" "set --erase \$1" | source
+    string collect (
+        bind --all | string replace --filter --regex -- _autopair.\* --erase
+        set --names | string replace --filter --regex -- \^autopair "set --erase autopair"
+    ) | source
+    functions --erase (functions --all | string match "_autopair_*")
 end
